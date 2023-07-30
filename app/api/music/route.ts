17a538d39cc1a3ffe9d2c11f0c9c2679ee332779
Aleksander <asava@sibmail.com>
2023-07-30 20:45:05 +0700

    Music Generation A

--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -4,7 +4,7 @@ import Replicate from 'replicate'
 
 const replicate = new Replicate({
   // auth: process.env.REPLICATE_API_TOKEN
-  // auth: process.env.REPLICATE_API_TOKEN || '' // 2:50:24
+  // auth: process.env.REPLICATE_API_TOKEN || ''
   auth: process.env.REPLICATE_API_TOKEN!
 })
 
@@ -12,27 +12,28 @@ export async function POST(req: Request) {
   try {
     const { userId } = auth()
     const body = await req.json()
-    const { messages } = body
+    const { prompt } = body
 
     if (!userId) {
       return new NextResponse('Unauthorized', { status: 401 })
     }
 
-    if (!configuration.apiKey) {
-      return new NextResponse('OpenAI API Key not configured', { status: 500 })
+    if (!prompt) {
+      return new NextResponse('Prompt are required', { status: 400 })
     }
 
-    if (!messages) {
-      return new NextResponse('Messages are required', { status: 400 })
-    }
+    const response = await replicate.run(
+      'riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05',
+      {
+        input: {
+          prompt_a: prompt
+        }
+      }
+    )
 
-    const response = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo',
-      messages
-    })
-    return NextResponse.json(response.data.choices[0].message)
+    return NextResponse.json(response)
   } catch (error) {
-    console.log('[CONVERSATION_ERROR]', error)
+    console.log('[MUSIK_ERROR]', error)
     return new NextResponse('internal error', { status: 500 })
   }
 }
