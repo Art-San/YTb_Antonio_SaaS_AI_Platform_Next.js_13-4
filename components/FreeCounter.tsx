5dea43f60e9d33f7be58b1be266500aa8ce5978f
Aleksander <asava@sibmail.com>
2023-08-01 23:12:13 +0700

    API Limit UI Counter

--- components/FreeCounter.tsx
+++ components/FreeCounter.tsx
@@ -3,6 +3,9 @@
 import { MAX_FREE_COUNTS } from '@/constans'
 import { useEffect, useState } from 'react'
 import { Card, CardContent } from './ui/card'
+import { Progress } from '@/components/ui/progress'
+import { Button } from '@/components/ui/button'
+import { Zap } from 'lucide-react'
 
 interface IFreeCounterProps {
   apiLimitCount: number
@@ -25,9 +28,17 @@ const FreeCounter = ({ apiLimitCount = 0 }: IFreeCounterProps) => {
         <CardContent className="py-6">
           <div className="text-center text-sm text-white mb-4 space-y-2">
             <p>
-              {apiLimitCount} / {MAX_FREE_COUNTS} 3:32:58
+              {apiLimitCount} / {MAX_FREE_COUNTS}
             </p>
+            <Progress
+              className="h3"
+              value={(apiLimitCount / MAX_FREE_COUNTS) * 100}
+            />
           </div>
+          <Button className="w-full" variant={'premium'}>
+            Upgrade
+            <Zap className="w-4 h-4 ml-2 fill-white" />
+          </Button>
         </CardContent>
       </Card>
     </div>
