08d17fd8650ca9bc91eb1e5cdfd687409d7a33d8
Aleksander <asava@sibmail.com>
2023-08-02 22:21:47 +0700

    Pro Modal UI

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,13 @@
+import { getApiLimitCount } from '@/lib/api-limit'
 import { UserButton } from '@clerk/nextjs'
 
 import MobileSidebar from './MobileSidebar'
 
-const Navbar = () => {
+const Navbar = async () => {
+  const apiLimitCount = await getApiLimitCount()
   return (
     <div className="flex items-center p-4">
-      <MobileSidebar />
+      <MobileSidebar apiLimitCount={apiLimitCount} />
       <div className=" flex w-full justify-end">
         <UserButton afterSignOutUrl="/" />
       </div>
